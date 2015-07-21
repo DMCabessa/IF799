9b28e8dec5901c75c537d0ccab92aff88c8a906b
Victor Pimenta <vfp@cin.ufpe.br>
2015-07-21 17:22:52 -0300

    Updated Constants.java to enable remote deploying.

--- UniversityOntology/src/util/Constants.java
+++ UniversityOntology/src/util/Constants.java
@@ -8,7 +8,12 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 
 public interface Constants {
-	String prefix = "/wtpwebapps/UniversityOntology/WEB-INF";
+	String windows_prefix = "/wtpwebapps/UniversityOntology/WEB-INF";
+	String linux_prefix = "/webapps/UniversityOntology/WEB-INF";
+	// Usage:
+	// * Local server (on Windows) -> windows_prefix
+	// * Remote server (either dev os sistemas) -> linux_prefix
+	String prefix = linux_prefix;
 	String sufix = "/owl/project_ontology.owl";
 	File catalinaBase = new File(System.getProperty("catalina.base")).getAbsoluteFile();
 	
